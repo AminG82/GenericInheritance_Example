773648646760119d8e32a559f46ff83ed6b6fab0
Amin Ghasemi <mmnghasemi@gmail.com>
2024-06-02 20:58:30 -0800

    Tests Class created to test inheritance for generic classec more info in comments !

---
+++ Models/Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericInheritance_Example.Models
+{
+    public class Tests
+    {
+        // Test the inheritance !
+    }
+
+    public class Test1<T>
+    {
+        //a generic Class can inheritance a normall class
+    }
+    public class Test2 : Test1<int>
+    {
+        // normal class cant inheritance an unkown generic class!
+        // but it will work if the datatype is cleard
+    }
+
+    public class Test3<T> : Test1<T>
+    {
+        //Generic Classes can inheritance each other !
+        // but the refrens should be the same even if it have multiple refrens like : <T , K , Z>
+    }
+
+}
