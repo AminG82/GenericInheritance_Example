663c742573d13e4c2d6296c3e00405857ca38990
Amin Ghasemi <mmnghasemi@gmail.com>
2024-06-02 20:34:42 -0800

    BaseClass is working well for Customer Class but not Product so we need some changes in BaseClass

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,9 +1,9 @@
 ﻿namespace GenericInheritance_Example.Models
 {
-    public class Customer
+    public class Customer : BaseClass
     {
-        public int Id { get; set; }
-        public DateTime CreationDate { get; set; }
+        //public int Id { get; set; }                   Using BaseClass to prevent Code repetition
+        //public DateTime CreationDate { get; set; }        
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,9 +1,9 @@
 ﻿namespace GenericInheritance_Example.Models
 {
-    public class Product
+    public class Product : BaseClass
     {
-        //public double Id { get; set; }
-        //public DateTime CreationDate { get; set; }        Using BaseClass to prevent 
+        //public double Id { get; set; }                Using BaseClass to prevent Code repetition
+        //public DateTime CreationDate { get; set; }        but there is a problem with Id datatype! its not int in both Entitys
         public string Name { get; set; }
         public string Description { get; set; }
 
